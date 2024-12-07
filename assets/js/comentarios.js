359820efd17d3abe8b6309acf6085ae0d0da7929
Tania <oert070904mmclyna2@soycecytem.mx>
2024-12-07 15:38:37 -0600

    ESTILO DE OPINIONES

--- assets/js/comentarios.js
+++ assets/js/comentarios.js
@@ -26,10 +26,9 @@ const cargarO = async () => {
         moment().format("L");
 
         var fecha3 = moment(opi.fecha, 'YYYY-MM-DD hh:mm:ss').fromNow();
-
         divO += `
-           <div class="card w-50 m-auto mt-3">
-    <div class="card-header">
+           <div class="card w-50 m-auto mt-3" style="border: 3px solid #8fbedc;">
+    <div class="card-header" style="background-color: #ceecff;">
         <div class="col m-2">
             <img src="${opi.foto}" width="50px" height="50px" style="border-radius: 100%;">
             <b class="mx-1">${opi.name}</b>
@@ -41,9 +40,9 @@ const cargarO = async () => {
         <p class="">${opi.opinion}</p>
     </div>
 
-    <div class="card-footer d-flex align-items-center justify-content-between">
+    <div class="card-footer d-flex align-items-center justify-content-between" style="background-color: #ceecff;">
         <input type="text" class="form-control d-inline-block w-75" placeholder="Agrega un comentario" id="comentario${opi.id_o}">
-        <button class="btn btn-primary mx-2" onclick="comentar(${opi.id_o},${opi.id_u})">
+        <button class="btn btn-primary mx-2" onclick="comentar(${opi.id_o},${opi.id_u})" style="background-color: #18c0ac;border-color:#18c0ac">
             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-send-fill" viewBox="0 0 16 16">
                 <path d="M15.964.686a.5.5 0 0 0-.65-.65L.767 5.855H.766l-.452.18a.5.5 0 0 0-.082.887l.41.26.001.002 4.995 3.178 3.178 4.995.002.002.26.41a.5.5 0 0 0 .886-.083zm-1.833 1.89L6.637 10.07l-.215-.338a.5.5 0 0 0-.154-.154l-.338-.215 7.494-7.494 1.178-.471z"/>
             </svg>
